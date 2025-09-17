e20c0aee10eb1091de730ba52c6e044f60935d23
Fnu Abdullah <abdullahmkh004@gmail.com>
2025-09-16 19:27:43 -0500

    Workflow updated and secrets relied on GitHub Secrets

--- encrypt_full.py
+++ encrypt_full.py
@@ -14,9 +14,11 @@ import sys
 
 # ---------- Supabase Setup ----------
 # Load .env
-load_dotenv()
+# load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise RuntimeError("Missing Supabase credentials")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # ---------- Crypto Helper ----------
